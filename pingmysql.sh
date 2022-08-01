97dc5a5bef0efe04f6986bca99ab261c398e79c8
Sarathvs14 <37289634+Sarathvs14@users.noreply.github.com>
2022-08-01 18:22:16 +0530

    mysql runnng status check

---
+++ pingmysql.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+str=''
+mysqladmin -uroot --port=5501 --protocol=tcp  ping &> /dev/null
+str=$?
+if [ "$str" -eq 0 ]; then
+  echo "mysql is runnnig";
+else 
+	echo "ALERT! MySQL Server is down";
+fi
